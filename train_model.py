b2dc4031d76bee348f94dd6a63238009157652c9
Crist96351318 <atoncristian2@gmail.com>
2026-05-04 11:06:36 +0800

    Fixing the bug

--- train_model.py
+++ train_model.py
@@ -17,7 +17,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = RandomForestClassifier(n_estimators=100)
 model.fit(X_train, y_train)
 
-# Save the model as a .pkl file (Required Product #1)
+# Save the model as a .pkl file 
 with open('model.pkl', 'wb') as f:
     pickle.dump(model, f)
 
